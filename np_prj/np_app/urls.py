fca2f3023c7deae8b762cce27c111e3443cef5eb
Vladislafff78 <vladislafff@gmail.com>
2023-01-28 13:04:11 +0300

    Module D5. Authorization and Registration

--- np_prj/np_app/urls.py
+++ np_prj/np_app/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', views.index, name='about'),
     path('posts', views.posts, name='posts'),
     path('create_post', views.CreatePost.as_view(), name='create_post'),
-    path('create_post', views.create_post, name='create_post'),
-
 
     ]
+
--- np_prj/np_app/views.py
+++ np_prj/np_app/views.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import (
-   ListView, DetailView, CreateView, UpdateView, DeleteView
-)
+from django.views.generic import (CreateView)
 from django.shortcuts import render, redirect
 
 from .models import Post
@@ -13,6 +11,12 @@ class CreatePost(LoginRequiredMixin, CreateView):
     form_class = PostForm
     model = Post
     template_name = 'create_post.html'
+    success_url = 'posts'
+
+    def create_post(self):
+        form = PostForm(self.request.POST)
+        form.save()
+        return redirect('posts')
 
 
 def index(request):
@@ -22,21 +26,3 @@ def index(request):
 def posts(request):
     post = Post.objects.order_by('-id')
     return render(request, 'posts.html', {'title': 'Новостной портал - Статьи', 'post': post})
-
-
-def create_post(request):
-    error = ''
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('posts')
-        else:
-            error = 'Что то пошло не так'
-
-    form = PostForm()
-    context = {
-        'form': form,
-        'error': error
-    }
-    return render(request, 'create_post.html', context)
